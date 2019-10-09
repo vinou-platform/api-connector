97340371b0ca219f65fda74291b87b2425ec04dd
Christian Händel <christian@vinou.de>
2019-10-10 00:43:59 +0200

    [BUGFIX] TYPO3 Session handling

--- Classes/Session/TYPO3Session.php
+++ Classes/Session/TYPO3Session.php
@@ -10,37 +10,50 @@ class TYPO3Session {
 	public static $storageKey = 'vinou-api-connector';
 
 	public static function readSessionData($key) {
-		$storage = self::getStoreObject();
-        $sessionData = $storage->getSessionData( self::$storageKey );
-        return isset( $sessionData[$key] ) ? $sessionData[$key] : false;
+		if (TYPO3_MODE === 'BE')
+			return self::readBESessionData($key);
+
+		if ($GLOBALS['TSFE']->loginUser) {
+		    return $GLOBALS['TSFE']->fe_user->getKey('user', $key);
+		} else {
+		    return $GLOBALS['TSFE']->fe_user->getKey('ses', $key);
+		}
 	}
 
 	public static function writeSessionData($key,$data) {
-		$storage = self::getStoreObject();
-        $sessionData = $storage->getSessionData( self::$storageKey );
-        $sessionData[$key] = $data;
-        return $storage->setAndSaveSessionData( self::$storageKey, $sessionData );
+		if (TYPO3_MODE === 'BE')
+			return self::writeBESessionData($key,$data);
+
+		if ($GLOBALS['TSFE']->loginUser) {
+			$GLOBALS['TSFE']->fe_user->setKey('user', $key, $data);
+		} else {
+			$GLOBALS['TSFE']->fe_user->setKey('ses', $key, $data);
+		}
+		return $GLOBALS['TSFE']->fe_user->storeSessionData();
 	}
 
 	public static function removeSessionData($key) {
-		$storage = self::getStoreObject();
-		return $storage->setAndSaveSessionData( self::$storageKey, [] );
+		if (TYPO3_MODE === 'BE')
+			return self::removeBESessionData($key);
+
+		unset($GLOBALS['TSFE']->fe_user->sesData[$key]);
+		return $GLOBALS['TSFE']->fe_user->storeSessionData();
 	}
 
-	public static function getStoreObject() {
-		switch (TYPO3_MODE) {
-			case 'FE':
-				return $GLOBALS['TSFE']->fe_user;
-				break;
 
-			case 'BE':
-				return $GLOBALS['BE_USER'];
-				break;
+	public static function readBESessionData($key) {
+        $sessionData = $GLOBALS['BE_USER']->getSessionData( self::$storageKey );
+        return isset( $sessionData[$key] ) ? $sessionData[$key] : false;
+	}
 
-			default:
-				throw new \Exception('TYPO3_MODE not defined');
-				exit;
-		}
+	public static function writeBESessionData($key,$data) {
+        $sessionData = $GLOBALS['BE_USER']->getSessionData( self::$storageKey );
+        $sessionData[$key] = $data;
+        return $GLOBALS['BE_USER']->setAndSaveSessionData( self::$storageKey, $sessionData );
+	}
+
+	public static function removeBESessionData($key) {
+		return $GLOBALS['BE_USER']->setAndSaveSessionData( self::$storageKey, [] );
 	}
 
 }
