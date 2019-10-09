04144eb65f5901414dbbe8ec36b6f62aebd6ccf7
Christian Händel <christian@vinou.de>
2019-10-09 17:21:17 +0200

    [BUGFIX] TYPO3 Session handling

--- Classes/Session/TYPO3Session.php
+++ Classes/Session/TYPO3Session.php
@@ -7,26 +7,40 @@ namespace Vinou\ApiConnector\Session;
 
 class TYPO3Session {
 
+	public static $storageKey = 'vinou-api-connector';
+
 	public static function readSessionData($key) {
-		if ($GLOBALS['TSFE']->loginUser) {
-		    return $GLOBALS['TSFE']->fe_user->getKey('user', $key);
-		} else {
-		    return $GLOBALS['TSFE']->fe_user->getKey('ses', $key);
-		}
+		$storage = self::getStoreObject();
+        $sessionData = $storage->getSessionData( self::$storageKey );
+        return isset( $sessionData[$key] ) ? $sessionData[$key] : false;
 	}
 
 	public static function writeSessionData($key,$data) {
-		if ($GLOBALS['TSFE']->loginUser) {
-			$GLOBALS['TSFE']->fe_user->setKey('user', $key, $data);
-		} else {
-			$GLOBALS['TSFE']->fe_user->setKey('ses', $key, $data);
-		}
-		return $GLOBALS['TSFE']->fe_user->storeSessionData();
+		$storage = self::getStoreObject();
+        $sessionData = $storage->getSessionData( self::$storageKey );
+        $sessionData[$key] = $data;
+        return $storage->setAndSaveSessionData( self::$storageKey, $sessionData );
 	}
 
 	public static function removeSessionData($key) {
-		unset($GLOBALS['TSFE']->fe_user->sesData[$key]);
-		return $GLOBALS['TSFE']->fe_user->storeSessionData();
+		$storage = self::getStoreObject();
+		return $storage->setAndSaveSessionData( self::$storageKey, [] );
+	}
+
+	public static function getStoreObject() {
+		switch (TYPO3_MODE) {
+			case 'FE':
+				return $GLOBALS['TSFE']->fe_user;
+				break;
+
+			case 'BE':
+				return $GLOBALS['BE_USER'];
+				break;
+
+			default:
+				throw new \Exception('TYPO3_MODE not defined');
+				exit;
+		}
 	}
 
 }
