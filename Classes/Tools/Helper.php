b974e49cf6c924b82b67308ca4cbae28cfb5e5a9
Christian Händel <christian@vinou.de>
2019-12-05 07:10:39 +0100

    [ADD] helper function to fetch image data

--- Classes/Tools/Helper.php
+++ Classes/Tools/Helper.php
@@ -75,4 +75,36 @@ class Helper {
         }
         return $searchArray;
     }
+
+    public static function curl_get_contents($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+
+    public static function imageToBase64($url){
+        $data = base64_encode(self::curl_get_contents($url));
+        $mime_types = array(
+            'pdf' => 'application/pdf',
+            'doc' => 'application/msword',
+            'odt' => 'application/vnd.oasis.opendocument.text ',
+            'docx'  => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            'gif' => 'image/gif',
+            'jpg' => 'image/jpg',
+            'jpeg' => 'image/jpeg',
+            'png' => 'image/png',
+            'bmp' => 'image/bmp'
+        );
+        $ext = pathinfo($url, PATHINFO_EXTENSION);
+
+        if (array_key_exists($ext, $mime_types)) {
+            $a = $mime_types[$ext];
+        }
+
+        return 'data: '.$a.';base64,'.$data;
+    }
 }
